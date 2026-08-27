1641d773649a7f9d58966b15f9392494c290acd0
sarenya-samapika <sarenyasamapika@gmail.com>
2026-08-27 11:14:21 +0530

    selection sort

---
+++ src/selectionSort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class selectionSort {
+    public static void main(String[] args){
+        int[] arr = {3, 1, 5, 4, 2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    private static int getMaxIndex(int[] arr, int start, int end){
+        int max = start;
+        for(int i = start; i <= end; i++){
+            if(arr[max]<arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swapArray(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]= temp;
+    }
+
+    static void selection(int[] arr){
+
+        for(int i=0; i< arr.length; i++){
+            int last = arr.length - i -1;
+            int max = getMaxIndex(arr, 0, last);
+            swapArray(arr, max, last);
+        }
+    }
+}
